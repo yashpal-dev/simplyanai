1f2a3bcb5ea84617f3040da614efd1dc4fa8fa50
Yashraj Singh <yashrajsinghji8@gmail.com>
2024-12-16 07:31:22 +0530

    Modified: bug fixed

--- app/buggy/page.tsx
+++ app/buggy/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function Page() {
   return (
     <>
+      {/* visible only on large screen sizes */}
       <div className="w-screen h-screen grid place-items-center font-extrabold text-xl text-center lg:hidden">
         This application is not usable in your device.
       </div>
